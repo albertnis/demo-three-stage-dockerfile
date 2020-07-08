98f376e0b17e69ada4f7173dabe69c771fa0439f
Albert Nisbet <albertnis@gmail.com>
2020-07-08 20:23:06 +1200

    Update helloworld.ts

--- helloworld.ts
+++ helloworld.ts
@@ -1,6 +1,6 @@
 import { argv } from 'process'
 
-const greet = (name: string): string => `Hello ${name}!`
+const greet = (name: string): string => `Hello ${name} from a PR!`
 
 const greeting = greet(argv.length > 2 ? argv[2] : 'unknown person')
 
