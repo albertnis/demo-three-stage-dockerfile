dffa9e443bb177b6d9044280fe775620e102a921
Albert Nisbet <albertnis@gmail.com>
2020-07-08 20:40:41 +1200

    Correct docker image name (again) and docker login

--- .github/actions/docker-push/entrypoint.sh
+++ .github/actions/docker-push/entrypoint.sh
@@ -2,7 +2,8 @@
 set -e
 VERSION=$1
 GH_TOKEN=$2
+IMAGE_NAME=docker.pkg.github.com/albertnis/demo-three-stage-dockerfile
 
-echo $GH_TOKEN | docker login docker.pkg.github.com -u albertnis --password-stdin
+docker login docker.pkg.github.com -u albertnis -p $GH_TOKEN
 
-docker build -t albertnis/mqcontrol:$VERSION -t albertnis/mqcontrol:latest --push .
+docker build -t $IMAGE_NAME:$VERSION -t $IMAGE_NAME:latest --push .
